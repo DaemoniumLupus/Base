62bc062425308a51cd621069b833b4984bf2c3d8
DaemoniumLupus <37290179+DaemoniumLupus@users.noreply.github.com>
2020-06-30 13:05:40 +0300

    Lupus
    Lupus

---
+++ Project1/Задание 3.2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("color 02");
+
+	int total = 0;
+	int totalStage;
+	int stageIn;
+	int stageOut;
+	int 
+
+	while
+
+	cout << "Прибываем на остановку Улица Програмистов. \nВ салоне пассажиров: " << total;
+
+
+	_getch();
+	return 0;
+}
